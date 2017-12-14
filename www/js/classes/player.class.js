c7ef25e66d7a0556e517437bd50c06ecb4c9d361
Dennis <dennisek85@gmail.com>
2017-12-14 15:04:59 +0100

    add player class

---
+++ www/js/classes/player.class.js
@@ -0,0 +1,12 @@
+class Player extends Base {
+
+    constructor( name, color, ai, difficulty ){
+        super();
+        this.name = name;
+        this.color = color;
+        this.ai = false;
+        this.difficulty = null;
+
+
+    }
+}
