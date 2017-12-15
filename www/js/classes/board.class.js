dfe44c6067558200960dffb9c7d403966115d02b
Shamin Krook <shahmineh.gh@hotmail.com>
2017-12-15 11:28:17 +0100

    Click event

--- www/js/classes/board.class.js
+++ www/js/classes/board.class.js
@@ -6,19 +6,24 @@ class Board {
     this.selector = selector;
     this.createBoard();
 
+    $(document).on('click mouseenter mouseleave','.slot',function(e){
+      let me = $(this);
+      let col = me.attr('data-col');
+      console.log('You have ' + e.type + ' column', col);
+    });
+
   }
 
-createBoard() {
-  const holder = $('.board');
-  for (let i = 0; i < this.rows; i++) {
-    let row = $('<div>').addClass('board-row');
-    for (let j = 0; j < this.cols; j++) {
-      let col = $('<div>').addClass('board-col empty').attr('data-row', i).attr('data-col', j);
-      row.append(col); // Magic
+  createBoard() {
+    const holder = $('.board');
+    for (let i = 0; i < this.rows; i++) {
+      let row = $('<div>').addClass('board-row');
+      for (let j = 0; j < this.cols; j++) {
+        let col = $('<div class="slot">').addClass('board-col empty').attr('data-row', i).attr('data-col', j);
+        row.append(col);
+      }
+      holder.append(row);
     }
-    holder.append(row);
   }
-  console.log(holder.html());  
-}
 
 }
